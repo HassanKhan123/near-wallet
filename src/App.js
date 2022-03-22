6be1c7e66cda2ad6c397ce0ac163ffca7cc0bb6a
HassanKhan123 <hk2602809@gmail.com>
2022-03-22 20:59:53 +0500

    added: import account from userId and fixed seed phrase generation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import { useEffect, useState } from "react";
 import { parseNearAmount } from "near-api-js/lib/utils/format";
 import BN from "bn.js";
 
-import { generateSeedPhrase } from "./utils";
+import {
+  generateSeed,
+  parseSeedPhrase,
+  parseSeedPhraseToAddress,
+} from "./utils";
+import "./App.css";
 
 function App() {
   const [id, setId] = useState("");
   const [publicKey, setPublickKey] = useState("");
   const [privateKey, setPrivatekKey] = useState("");
+  const [accoundID, setAccountID] = useState("");
+  const [phrase, setPhrase] = useState("");
+  const [initialMnemonic, setInitialMnemonic] = useState("");
 
   const [near, setNear] = useState({});
   const [balance, setBalance] = useState(0);
@@ -28,11 +36,19 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const phrase = generateSeedPhrase();
-    console.log("PHRASE===========", phrase);
-    setPublickKey(phrase.publicKey);
-    setPrivatekKey(phrase.secretKey);
+    const { phrase, address, secret } = generateSeed();
+    console.log("PSJ====", phrase, address, secret);
+
+    setPublickKey(address);
+    setPrivatekKey(secret);
+    setInitialMnemonic(phrase);
   }, []);
+
+  const grantPermission = () => {
+    const { publicKey, secretKey } = parseSeedPhraseToAddress(phrase);
+    setPublickKey(publicKey);
+    setPrivatekKey(secretKey);
+  };
 
   const checkAccountStatus = async accountInfo => {
     try {
@@ -49,6 +65,19 @@ function App() {
     const balance = await account.getAccountBalance();
     console.log("BAL======", balance);
     setBalance(balance.available / 10 ** 24);
+    console.log(
+      await account.getAccountDetails(),
+      await account.getAccessKeys()
+    );
+  };
+  // pupil method interest album short combine spend betray climb remove analyst swift
+
+  const importAccount = async () => {
+    const newAccount = await near.account(accoundID);
+
+    const balance = await newAccount.getAccountBalance();
+    console.log(await newAccount.getAccessKeys());
+    setBalance(balance.available / 10 ** 24);
   };
 
   const createAccount = async () => {
@@ -64,42 +93,66 @@ function App() {
     }
   };
 
+  const checkPermission = async account => {
+    let accessKeys = await account.getAccessKeys();
+    if (accessKeys[0].access_key.permission !== "FullAccess") {
+      return false;
+    }
+    return true;
+  };
+
   const sendFunds = async () => {
-    let sender = "puranaoffice.testnet";
-    let networkId = "testnet";
-    const keyStore = new keyStores.InMemoryKeyStore();
-    console.log(":KEY========", keyStore, privateKey);
-    const keyPair = KeyPair.fromString(privateKey);
-    await keyStore.setKey("testnet", sender, keyPair);
+    try {
+      let receiver = "puranaoffice.testnet";
+      let networkId = "testnet";
+      const keyStore = new keyStores.InMemoryKeyStore();
+      console.log(":KEY========", keyStore, privateKey);
+      const keyPair = KeyPair.fromString(privateKey);
+      await keyStore.setKey("testnet", accoundID, keyPair);
 
-    const config = {
-      networkId,
-      keyStore,
-      nodeUrl: `https://rpc.${networkId}.near.org`,
-      walletUrl: `https://wallet.${networkId}.near.org`,
-      helperUrl: `https://helper.${networkId}.near.org`,
-      explorerUrl: `https://explorer.${networkId}.near.org`,
-    };
+      const config = {
+        networkId,
+        keyStore,
+        nodeUrl: `https://rpc.${networkId}.near.org`,
+        walletUrl: `https://wallet.${networkId}.near.org`,
+        helperUrl: `https://helper.${networkId}.near.org`,
+        explorerUrl: `https://explorer.${networkId}.near.org`,
+      };
 
-    // connect to NEAR! :)
-    const near = await connect(config);
-    // create a NEAR account object
-    const senderAccount = await near.account(sender);
+      // connect to NEAR! :)
+      const near = await connect(config);
+      // create a NEAR account object
+      const senderAccount = await near.account(accoundID);
 
-    const yoctoAmount = parseNearAmount("5");
-    const amount = new BN(yoctoAmount);
-    const transaction = await senderAccount.sendMoney("pizza.testnet", amount);
-    console.log("HASH========", transaction.transaction.hash);
+      const yoctoAmount = parseNearAmount("5");
+      const amount = new BN(yoctoAmount);
+      const transaction = await senderAccount.sendMoney(receiver, amount);
+      console.log("HASH========", transaction.transaction.hash);
+    } catch (error) {
+      console.log("err=============", error.message);
+      if (
+        error.message.includes("no matching key pair found in InMemorySigner")
+      ) {
+        alert("You dont have permission");
+      }
+    }
   };
 
   return (
     <div className="App">
+      <h1>Seed Phrase: {initialMnemonic}</h1>
       <input value={id} onChange={e => setId(e.target.value)} />
       <button onClick={createAccount}>Create Account</button>
 
       <p>Your balance is {balance}</p>
 
       <button onClick={sendFunds}>Send Funds</button>
+
+      <input value={accoundID} onChange={e => setAccountID(e.target.value)} />
+      <button onClick={importAccount}>Import Account</button>
+
+      <input value={phrase} onChange={e => setPhrase(e.target.value)} />
+      <button onClick={grantPermission}>Grant permission</button>
     </div>
   );
 }
--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,27 @@
-import { derivePath } from "near-hd-key";
+import { derivePath } from "ed25519-hd-key";
 import bs58 from "bs58";
 import nacl from "tweetnacl";
 import * as bip39 from "bip39";
+import { hdkey } from "ethereumjs-wallet";
+import { ethers } from "ethers";
+import { KeyPair } from "near-api-js";
+import { generateSeedPhrase, parseSeedPhrase } from "near-seed-phrase";
 
+// const KEY_DERIVATION_PATH = `m/44'/60'/0'/0/0`;
 const KEY_DERIVATION_PATH = "m/44'/397'/0'";
 
-export const generateSeedPhrase = entropy => {
-  return parseSeedPhrase(
-    entropy !== undefined
-      ? bip39.entropyToMnemonic(entropy)
-      : // : bip39.generateMnemonic()
-        "twelve then small nut cry maid faith hair rabbit own employ agree"
-  );
+export const generateSeed = entropy => {
+  const { seedPhrase, publicKey, secretKey } = generateSeedPhrase();
+  const recoveryKeyPair = KeyPair.fromString(secretKey);
+  return {
+    phrase: seedPhrase,
+    address: recoveryKeyPair.getPublicKey().toString(),
+    secret: recoveryKeyPair.secretKey,
+  };
 };
+
+// taste hybrid oppose tent eye deer west uncle behind choose claim impact
+// pleasemeribaatsuno.testnet
 
 const normalizeSeedPhrase = seedPhrase =>
   seedPhrase
@@ -21,16 +30,20 @@ const normalizeSeedPhrase = seedPhrase =>
     .map(part => part.toLowerCase())
     .join(" ");
 
-export const parseSeedPhrase = (seedPhrase, derivationPath) => {
-  const seed = bip39.mnemonicToSeed(normalizeSeedPhrase(seedPhrase));
-  const { key } = derivePath(
-    derivationPath || KEY_DERIVATION_PATH,
-    seed.toString("hex")
-  );
-  const keyPair = nacl.sign.keyPair.fromSeed(key);
-  const publicKey = "ed25519:" + bs58.encode(Buffer.from(keyPair.publicKey));
-  const secretKey = "ed25519:" + bs58.encode(Buffer.from(keyPair.secretKey));
-  return { seedPhrase, secretKey, publicKey };
+export const parseSeedPhraseToAddress = (phrase, derivationPath) => {
+  const { publicKey, secretKey, seedPhrase } = parseSeedPhrase(phrase);
+  console.log("recover====", publicKey, secretKey, seedPhrase);
+  return { publicKey, secretKey };
+
+  // const seed = bip39.mnemonicToSeed(normalizeSeedPhrase(phrase));
+  // const { key } = derivePath(
+  //   derivationPath || KEY_DERIVATION_PATH,
+  //   seed.toString("hex")
+  // );
+  // const keyPair = nacl.sign.keyPair.fromSeed(key);
+  // const publicKey = "ed25519:" + bs58.encode(Buffer.from(keyPair.publicKey));
+  // const secretKey = "ed25519:" + bs58.encode(Buffer.from(keyPair.secretKey));
+  // return { phrase, secretKey, publicKey };
 };
 
 export const findSeedPhraseKey = (seedPhrase, publicKeys) => {
